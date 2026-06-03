8821a71fdaf9d32aa1355f3f3fb1bf52c06e8d94
CnBarrier <CnBarrier0921@outlook.com>
2026-06-03 23:44:13 +0800

    build(project): place TPK resource beside executable

--- src/UnityAssetsPatcher/Program.cs
+++ src/UnityAssetsPatcher/Program.cs
@@ -8,7 +8,7 @@ public static class Program
     {
         // TPK 是随程序分发的类型数据库资源，不依赖启动时的工作目录
         // 获取：https://github.com/AssetRipper/Tpk
-        string tpkFilePath = Path.Combine(AppContext.BaseDirectory, "Assets", "AssetsRipper.tpk");
+        string tpkFilePath = Path.Combine(AppContext.BaseDirectory, "resources.tpk");
 
         var assetsTools = new AssetsToolsReader(tpkFilePath);
         string backupDirectory = Path.Combine(AppContext.BaseDirectory, "backup");
